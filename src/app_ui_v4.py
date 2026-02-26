431bd27dd3a122d07aec25529d7ad18dff87317b
Rachit Rahaman <rachitrahaman0@gmail.com>
2026-02-26 23:49:40 +0530

    Refactor sidebar HTML for improved styling
    Updated HTML structure for sidebar content.

--- src/app_ui_v4.py
+++ src/app_ui_v4.py
@@ -125,7 +125,7 @@ with st.sidebar:
 
     st.image("data/IMG-20260226-WA0017-removebg-preview.png", width=170)
     st.markdown("""
-<div style='text-align: center; padding: 10px;'>
+<div style='text-align: center; padding: 0;'>
     <h4 style='margin-top: 10px;'>SVU Medical AI</h4>
     <p style='font-size: 12px; opacity: 0.8;'>
         Developed by:<br>
